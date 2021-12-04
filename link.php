d2b82fe06922ecd4557684aeb27c1390db583414
Vashni Boamah <53240010+Vashni12@users.noreply.github.com>
2021-12-04 12:25:21 -0600

    simple form

---
+++ link.php
@@ -0,0 +1,13 @@
+<?php
+	$firstname = $_POST['firstname'];
+	$surname = $_POST['Surname'];
+	$city = $_POST['city'];
+	$state = $_POST['state'];
+	$location = $_POST['location'];
+	$visitor_email = $_POST['email'];
+	$college = $_POST['college'];
+	$area_code = $_POST['area_code'];
+	$phone_number = $_POST['phone_number'];
+
+
+?>
